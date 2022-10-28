55236d65c6f7e5cb707159e61b7764ea2e23ba67
Marco Silveira <marcosilveira39@gmail.com>
2022-10-28 17:51:03 -0300

    novos ajustes

--- node/index.js
+++ node/index.js
@@ -9,14 +9,21 @@
         password: 'root',
         database: 'nodedb'
     }
+    const connection = mysql.createConnection(config)
+    var sql = "CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name VARCHAR(255), primary key(id))"
+    connection.query(sql)
+    sql = `INSERT INTO people(name) values('Wesley')`
+    connection.query(sql)
+    connection.end()
 
     app.get('/', (req,res) => {
         const connection = mysql.createConnection(config)
-        const sql = `SELECT * FROM people`
+        sql = `SELECT * FROM people`
         connection.query(sql, function(err, result, fields){
-            Object.keys(result).forEach(function(key){
+            Object.keys(result).every(function(key){
                 var row = result[key]
                 res.send('<h1>Full Cycle Rocks!</h1></br>'+row.name)
+                return false
             })
         })
         connection.end()
